da22f82439a50e860e3ac4b7f56efbc2b84d0126
nkartikaxioned <kartikn@axioned.com>
2023-11-01 13:57:22 +0530

    js add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-/* Author: 
+const hamburger = document.querySelector('.hamburger'),
+html = document.querySelector('html'),
+hambarfirst = document.querySelector('.bar.first'),
+hambarsecond = document.querySelector('.bar.second'),
+hambarthird = document.querySelector('.bar.third'),
+nav = document.querySelector('nav'),
+navbar = document.querySelectorAll('nav ul li');
 
-*/
 
+
+hamburger.addEventListener('click', () => {
+
+    hambarfirst.classList.toggle('active1');
+    hambarsecond.classList.toggle('active2');
+    hambarthird.classList.toggle('active3');
+    nav.classList.toggle('active-nav');
+    html.classList.toggle('html-scroll')
+})
 
 
 
